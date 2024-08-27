3895281b6ee49c49777f4757bf92d894c43d62a2
root <root@APP-NEW-MAGENTO-DEV.l5pcvpfyma1uhfd5kokfdkyerb.bx.internal.cloudapp.net>
2024-08-27 03:01:40 +0000

    first commit

---
+++ app/design/frontend/Alothemes/geckoVertical/requirejs-config.js
@@ -0,0 +1,25 @@
+/*
+* @Author: nguyen
+* @Date:   2019-06-03 14:48:52
+* @Last Modified by:   nguyen
+* @Last Modified time: 2019-06-03 14:53:10
+*/
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/minicart': {
+                'js/mixins/cart-updated': true
+            }
+        }
+    },
+
+    paths: {
+		'alotheme/multiscroll': 'js/multiscroll/jquery.multiscroll.min'
+	},
+
+	shim: {
+		'alotheme/multiscroll': {
+			deps: ['jquery']
+		}
+	}
+};
